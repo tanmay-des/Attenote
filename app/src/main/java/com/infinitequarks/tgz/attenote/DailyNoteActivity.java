8126839d425f16116f885710c9a08f2ecde1fa39
Tanmay Singh <tanmaysingh1226@gmail.com>
2016-10-23 04:17:26 +0530

    Added Daily Notes Viewer according to the date through Database

--- app/src/main/java/com/infinitequarks/tgz/attenote/DailyNoteActivity.java
+++ app/src/main/java/com/infinitequarks/tgz/attenote/DailyNoteActivity.java
@@ -133,7 +133,7 @@ public class DailyNoteActivity extends AppCompatActivity {
 
 
                     } catch (ParseException e) {
-                        e.printStackTrace();
+                        e.printStackTrace() ;
                     }
                 }
 
